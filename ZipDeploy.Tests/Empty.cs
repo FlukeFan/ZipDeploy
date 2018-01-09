78d5d7428c03ae4c509e775eea9431ff16bee340
Richard Brown <fluke_fan@hotmail.com>
2018-01-09 20:17:07 +0000

    Added empty test to get CI up and running

---
+++ ZipDeploy.Tests/Empty.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ZipDeploy.Tests
+{
+    [TestFixture]
+    public class Empty
+    {
+        [Test]
+        public void NoTest()
+        {
+            "Verifying NUnit tests are wired up correctly".Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
