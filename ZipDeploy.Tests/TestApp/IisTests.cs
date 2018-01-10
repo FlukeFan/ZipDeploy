d709842ef3426c8195db0a83d71256d3a18beb38
Richard Brown <fluke_fan@hotmail.com>
2018-01-10 09:38:05 +0000

    Removed site after test completion (just to tidy up after ourselves)

--- ZipDeploy.Tests/TestApp/IisTests.cs
+++ ZipDeploy.Tests/TestApp/IisTests.cs
@@ -15,21 +15,40 @@ namespace ZipDeploy.Tests.TestApp
             Directory.CreateDirectory(iisFolder);
 
             CreateIisSite(iisFolder);
+
+            DeleteIisSite();
         }
+
+        #region IIS
+
+        private const string    c_iisName = "ZipDeployTestApp";
+        private const int       c_iisPort = 8099;
 
         private void CreateIisSite(string iisFolder)
         {
-            var iisManager = new ServerManager();
-
-            var name = "ZipDeployTestApp";
-
-            if (iisManager.Sites.Count(s => s.Name == name) == 0)
+            using (var iisManager = new ServerManager())
             {
-                iisManager.Sites.Add("ZipDeployTestApp", iisFolder, 8099);
-                iisManager.CommitChanges();
-            }
+                if (iisManager.Sites.Count(s => s.Name == c_iisName) == 0)
+                {
+                    iisManager.Sites.Add("ZipDeployTestApp", iisFolder, c_iisPort);
+                    iisManager.CommitChanges();
+                }
 
-            Test.WriteProgress($"Created IIS site {name}:8099 in {iisFolder}");
+                Test.WriteProgress($"Created IIS site {c_iisName}:{c_iisPort} in {iisFolder}");
+            }
         }
+
+        private void DeleteIisSite()
+        {
+            using (var iisManager = new ServerManager())
+            {
+                var site = iisManager.Sites.SingleOrDefault(s => s.Name == c_iisName);
+                iisManager.Sites.Remove(site);
+                iisManager.CommitChanges();
+                Test.WriteProgress($"Removed IIS set {c_iisName}");
+            }
+        }
+
+        #endregion
     }
 }
