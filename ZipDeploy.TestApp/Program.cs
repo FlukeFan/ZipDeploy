2a1db88b518a2be1776894df33547676b0d79c76
Richard Brown <fluke_fan@hotmail.com>
2018-01-12 17:34:14 +0000

    Added basic NLog logging

--- ZipDeploy.TestApp/Program.cs
+++ ZipDeploy.TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using NLog.Web;
 
 namespace ZipDeploy.TestApp
 {
@@ -7,11 +8,13 @@ namespace ZipDeploy.TestApp
     {
         public static void Main(string[] args)
         {
+            NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger().Info("Logging configured");
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseNLog()
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
--- ZipDeploy/ZipDeploy.cs
+++ ZipDeploy/ZipDeploy.cs
@@ -1,17 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace ZipDeploy
 {
     public class ZipDeploy
     {
-        private RequestDelegate _next;
-        private string          _restartUrl;
+        private ILogger<ZipDeploy>  _log;
+        private RequestDelegate     _next;
+        private string              _restartUrl;
 
-        public ZipDeploy(RequestDelegate next, ZipDeployOptions options)
+        public ZipDeploy(RequestDelegate next, ILogger<ZipDeploy> log, ZipDeployOptions options)
         {
+            _log = log;
             _next = next;
             _restartUrl = options.RestartUrl;
+
+            _log.LogInformation($"ZipDeploy started [RestartUrl={_restartUrl}]");
         }
 
         public async Task Invoke(HttpContext context)
