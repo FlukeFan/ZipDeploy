ecf11b8dd9316d44af3067c75501a889a81547f5
Richard Brown <fluke_fan@hotmail.com>
2018-01-17 17:08:23 +0000

    Turned off logging exceptions (might be causing AppVeyor failures)

--- ZipDeploy.TestApp/Program.cs
+++ ZipDeploy.TestApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using NLog;
 using NLog.Web;
 
 namespace ZipDeploy.TestApp
@@ -9,7 +8,7 @@ namespace ZipDeploy.TestApp
     {
         public static void Main(string[] args)
         {
-            LogManager.ThrowExceptions = true;
+            //LogManager.ThrowExceptions = true;
             NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger().Info("Logging configured");
             BuildWebHost(args).Run();
         }
