07e288b45b526b9927e215f0270bf194e8d51d3d
Richard Brown <fluke_fan@hotmail.com>
2018-01-30 07:16:20 +0000

    Fixed bt and btw doskey shortcuts

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -3,9 +3,9 @@
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 type readme.md
 @doskey bc=dotnet clean
-@doskey btw=dotnet watch msbuild /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bt=dotnet msbuild /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bw=dotnet watch msbuild $*
+@doskey btw=dotnet watch msbuild /p:FilterTest="test =~ $1" /p:NoCoverage=true $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bt=dotnet msbuild /p:FilterTest="test =~ $1" /p:NoCoverage=true $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bw=dotnet watch msbuild /p:FilterTest="cat != Slow" $*
 @doskey ba=dotnet msbuild $*
 @doskey b=dotnet msbuild /p:FilterTest="cat != Slow" $*
 @doskey br=dotnet restore $*
