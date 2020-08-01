3b7c581aba1edfd3faba60edbb55038be7d46d14
Richard Brown <fluke_fan@hotmail.com>
2020-08-01 11:45:19 +0100

    Updated report generator.

--- Build/ColorExec.cs
+++ Build/ColorExec.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -14,18 +15,26 @@ namespace Build
 
         public override bool Execute()
         {
-            Log.LogMessage(MessageImportance.Normal, $"ColorExec WorkingDirectory='{WorkingDirectory}' FileName='{FileName}' Arguments='{Arguments}'");
-
-            using (var process = new Process())
+            try
             {
-                process.StartInfo.FileName = FileName;
-                process.StartInfo.Arguments = Arguments;
-                process.StartInfo.WorkingDirectory = WorkingDirectory;
-                process.StartInfo.UseShellExecute = false;
-                process.Start();
-                process.WaitForExit();
+                Log.LogMessage(MessageImportance.Normal, $"ColorExec WorkingDirectory='{WorkingDirectory}' FileName='{FileName}' Arguments='{Arguments}'");
 
-                return process.ExitCode == 0;
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = FileName;
+                    process.StartInfo.Arguments = Arguments;
+                    process.StartInfo.WorkingDirectory = WorkingDirectory;
+                    process.StartInfo.UseShellExecute = false;
+                    process.Start();
+                    process.WaitForExit();
+
+                    return process.ExitCode == 0;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.LogError($"Error: {e}");
+                return false;
             }
         }
     }
