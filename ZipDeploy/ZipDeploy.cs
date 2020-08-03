98ba7932144d0f96221a4ff512a4df32d9cb9dc4
Richard Brown <fluke_fan@hotmail.com>
2020-08-03 17:15:09 +0100

    Removed more obsolete code.

--- ZipDeploy/ZipDeploy.cs
+++ ZipDeploy/ZipDeploy.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.IO;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace ZipDeploy
 {
     public class ZipDeploy
     {
+        public static ILoggerFactory LoggerFactory = new LoggerFactory();
+
         private static ILogger<ZipDeploy> _logger;
 
         public static void Run(Action<ZipDeployOptions> setupOptions, Action program)
@@ -60,86 +60,6 @@ namespace ZipDeploy
         public static ZipDeployOptions DefaultOptions()
         {
             return new ZipDeployOptions();
-        }
-
-        private enum State
-        {
-            Idle,
-            FoundZip,
-            UnzippingBinaries,
-            AwaitingRestart,
-        }
-
-        public static ILoggerFactory LoggerFactory = new LoggerFactory();
-
-        private object              _stateLock      = new object();
-        private State               _installState;
-        private ILogger<ZipDeploy>  _log;
-        private RequestDelegate     _next;
-        private ZipDeployOptions    _options;
-
-        public ZipDeploy(RequestDelegate next, ZipDeployOptions options)
-        {
-            _log = LoggerFactory.CreateLogger<ZipDeploy>();
-            _next = next;
-            _options = options;
-
-            _log.LogInformation($"ZipDeploy started "
-                + $"[IisUrl={_options.IisUrl}] "
-                + $"[NewPackageFileName={_options.NewPackageFileName}] "
-                + $"[LegacyTempFileName={_options.LegacyTempFileName}] "
-                + $"[DeployedPackageFileName={_options.DeployedPackageFileName}] "
-                + $"[HashesFileName={_options.HashesFileName}] "
-                + $"[IgnoredPaths={string.Join(", ", _options.PathsToIgnore)}] "
-                + $"[UserDomainName={Environment.UserDomainName}] [UserName={Environment.UserName}]");
-
-            //CompleteInstallation();
-
-            _installState = State.Idle;
-
-            //StartWatchingForInstaller();
-            DetectInstaller();
-        }
-
-        private void InstallBinaries()
-        {
-            _log.LogDebug("Installing binaries (and renaming old ones)");
-            var unzipper = new Unzipper(_options);
-            unzipper.UnzipBinaries();
-        }
-
-        //private void CompleteInstallation()
-        //{
-        //    if (File.Exists(_options.TempZipFileName))
-        //    {
-        //        _log.LogDebug($"detected {_options.TempZipFileName}; completing installation");
-
-        //        var unzipper = new Unzipper(_options);
-        //        unzipper.SyncNonBinaries();
-        //    }
-        //}
-
-        private void DetectInstaller()
-        {
-            if (_installState != State.Idle)
-                return;
-
-            lock (_stateLock)
-            {
-                if (_installState != State.Idle)
-                    return;
-
-                if (NewZipFileExists())
-                {
-                    _log.LogDebug("Detected installer");
-                    _installState = State.FoundZip;
-                }
-            }
-        }
-
-        private bool NewZipFileExists()
-        {
-            return File.Exists(_options.NewPackageFileName);
         }
     }
 }
