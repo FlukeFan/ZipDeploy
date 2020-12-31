78a8b2dae06fb5956942e95f52d7f0f2486b84d9
Richard Brown <fluke_fan@hotmail.com>
2020-12-31 10:55:32 +0000

    Added logging to 3.1 demo.

--- ZipDeploy.TestApp3_1/Program.cs
+++ ZipDeploy.TestApp3_1/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using NLog;
+using NLog.Common;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
 
 namespace ZipDeploy.TestApp3_1
 {
@@ -7,11 +12,24 @@ namespace ZipDeploy.TestApp3_1
     {
         public static void Main(string[] args)
         {
+            InternalLogger.LogFile = "logs\\nlog.internal.log";
+            InternalLogger.LogLevel = LogLevel.Info;
+
+            LogManager.ThrowExceptions = true;
+            var config = new LoggingConfiguration();
+            var logFile = new FileTarget("fileTarget") { FileName = "logs\\nlog.log" };
+            config.AddTarget(logFile);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logFile);
+
+            LogManager.ThrowExceptions = true;
+            LogManager.Configuration = config;
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseNLog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
